54bbf2c0c45ac355731cfe4eb98a34b924aa574e
cho-hadam <s2019w16@e-mirim.hs.kr>
2020-10-26 02:18:19 +0900

    publish 오류 수정

--- blog/views.py
+++ blog/views.py
@@ -44,7 +44,7 @@ def post_edit(request, pk):
 @login_required
 def post_draft_list(request):
     posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
-    return render(request, 'blog/post_draft_list.html', {'post': posts})
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
 
 def post_publish(request, pk):
     post = get_object_or_404(Post, pk=pk)
